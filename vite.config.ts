f8fba8aa8002f4c5f33b6f0e3affd701e1b629a2
Guo Yunhe <i@guoyunhe.me>
2022-10-30 22:03:03 +0800

    eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    open: true,
+  },
   plugins: [react()],
   test: {
     // https://vitest.dev/config/
