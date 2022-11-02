2cdd0c83ff8f4669111a0b78f5ca22f98a8cbb93
Guo Yunhe <i@guoyunhe.me>
2022-11-02 21:34:05 +0800

    test

---
+++ src/App.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, it } from 'vitest';
+import App from './App';
+
+describe('Simple working test', () => {
+  it('the title is visible', () => {
+    render(<App />);
+    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeTruthy();
+  });
+
+  it('should increment count on click', async () => {
+    render(<App />);
+    userEvent.click(screen.getByRole('button'));
+    expect(await screen.findByText(/count is: 1/i)).toBeTruthy();
+  });
+
+  it('uses flexbox in app header', async () => {
+    render(<App />);
+    const element = screen.getByRole('banner');
+    expect(element.className).toEqual('App-header');
+    expect(getComputedStyle(element).display).toEqual('flex');
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,6 @@ export default defineConfig({
     },
     globals: true,
     environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
   },
 });
