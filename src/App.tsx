acc57a0f04f9c7980f0251f3f141bf895df363e1
Guo Yunhe <i@guoyunhe.me>
2023-02-26 21:51:45 +0800

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { CssBaseline } from '@mui/material';
+import axios from 'axios';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { SWRConfig } from 'swr';
-import { apiSWRFetcher } from './api';
 import LanguageEffect from './components/LanguageEffect';
 import routes from './routes';
 
 const router = createBrowserRouter(routes);
 
+const fetcher = (url: string) => axios.get(url).then(res => res.data)
+
 export default function App() {
   return (
-    <SWRConfig value={{ fetcher: apiSWRFetcher }}>
-      <CssBaseline>
-        <RouterProvider router={router} />
-      </CssBaseline>
+    <SWRConfig value={{ fetcher }}>
+      <RouterProvider router={router} />
       <LanguageEffect />
     </SWRConfig>
   );
