bfc18849e115407ed4edd775437e582dfd8fcbc0
Guo Yunhe <i@guoyunhe.me>
2023-03-11 10:09:14 +0800

    fix landing layout

--- src/layouts/landing/Footer.tsx
+++ src/layouts/landing/Footer.tsx
@@ -1,21 +1,27 @@
-import { Box, Stack } from '@mui/material';
+import { Box, colors, Link, Stack } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 
 const date = new Date();
 
 export default function Footer() {
   const { t } = useTranslation();
   return (
-    <Box component="footer" p={3} display="flex">
+    <Box component="footer" p={3} display="flex" fontSize="14px" color={colors.grey[700]}>
       <Box>
         &copy; {date.getFullYear()} {import.meta.env.VITE_APP_NAME}
       </Box>
       <Box flex="1 1 auto" />
       <Stack direction="row" spacing={2}>
-        <Link to="/terms">{t('Terms')}</Link>
-        <Link to="/privacy">{t('Privacy')}</Link>
-        <Link to="/about">{t('About')}</Link>
+        <Link component={RouterLink} to="/terms" color="inherit" underline="hover">
+          {t('Terms')}
+        </Link>
+        <Link component={RouterLink} to="/privacy" color="inherit" underline="hover">
+          {t('Privacy')}
+        </Link>
+        <Link component={RouterLink} to="/about" color="inherit" underline="hover">
+          {t('About')}
+        </Link>
       </Stack>
     </Box>
   );
