d2731ef5a5ab74aecdf7d86b836f170c31da191e
Guo Yunhe <i@guoyunhe.me>
2023-03-14 21:24:38 +0800

    fix: language

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { AuthProvider } from '@guoyunhe/react-auth';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LanguageEffect from './components/LanguageEffect';
 import routes from './routes';
 import theme from './theme';
 
@@ -13,7 +12,6 @@ export default function App() {
       <AuthProvider>
         <CssBaseline />
         <RouterProvider router={router} />
-        <LanguageEffect />
       </AuthProvider>
     </ThemeProvider>
   );
