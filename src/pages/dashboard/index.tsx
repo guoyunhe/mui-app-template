c0be878e6a28f5f5bded1c0b25c13ac0897d830c
Guo Yunhe <i@guoyunhe.me>
2023-05-28 21:34:00 +0800

    dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,26 +1,3 @@
-import { useAuth } from '@guoyunhe/react-auth';
-import axios from 'axios';
-
 export default function DashboardPage() {
-  const auth = useAuth();
-  return (
-    <div>
-      <img src={auth.user?.avatar?.url} />
-      <input
-        type="file"
-        onChange={(e) => {
-          const avatar = e.target.files?.[0];
-          if (avatar) {
-            const data = new FormData();
-            data.append('avatar', avatar);
-            axios
-              .put('/user', data, { headers: { 'Content-Type': 'multipart/form-data' } })
-              .then((res) => {
-                auth.setUser(res.data);
-              });
-          }
-        }}
-      />
-    </div>
-  );
+  return <div>TODO</div>;
 }
