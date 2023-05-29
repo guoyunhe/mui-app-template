cb10beb9fabca5a2e463d912eec40f8325a8972b
Guo Yunhe <i@guoyunhe.me>
2023-05-29 22:11:49 +0800

    responsive font sizes

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,6 @@
+import { responsiveFontSizes } from '@mui/material';
 import { createCompactTheme } from 'mui-material-compact';
 
-const theme = createCompactTheme({});
+const theme = responsiveFontSizes(createCompactTheme({}), { factor: 3 });
 
 export default theme;
