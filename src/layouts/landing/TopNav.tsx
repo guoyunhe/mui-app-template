06946ea88e3245e279c1c0b1032766e2d8a586e4
Guo Yunhe <i@guoyunhe.me>
2023-06-17 22:14:46 +0800

    logged in navbar

--- src/layouts/landing/TopNav.tsx
+++ src/layouts/landing/TopNav.tsx
@@ -1,15 +1,19 @@
+import { AuthStatus, useAuth, useLogout } from '@guoyunhe/react-auth';
 import {
   AutoAwesome as AutoAwesomeIcon,
+  Dashboard,
   Login as LoginIcon,
+  Logout as LogoutIcon,
   Menu as MenuIcon,
   PersonAdd as PersonAddIcon,
-  Sell as SellIcon,
   SupportAgent as SupportAgentIcon,
 } from '@mui/icons-material';
-import { AppBar, Box, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { AppBar, Avatar, Box, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
 import LanguageMenu from 'src/components/language-menu';
+import User from 'src/types/models/User';
 
 export interface TopNavProps {
   onMenuButtonClick: () => void;
@@ -17,6 +21,8 @@ export interface TopNavProps {
 
 export default function TopNav({ onMenuButtonClick }: TopNavProps) {
   const { t } = useTranslation();
+  const auth = useAuth<User>();
+  const logout = useLogout();
 
   return (
     <AppBar position="static" color="transparent" elevation={0}>
@@ -56,46 +62,81 @@ export default function TopNav({ onMenuButtonClick }: TopNavProps) {
           <Button
             variant="text"
             color="inherit"
-            component={NavLink}
-            to="/pricing"
-            startIcon={<SellIcon />}
-          >
-            {t('Pricing')}
-          </Button>
-          <Button
-            variant="text"
-            color="inherit"
-            component={NavLink}
-            to="/support"
             startIcon={<SupportAgentIcon />}
+            component="a"
+            href="https://t.me/resume_maker_support"
+            target="_blank"
           >
             {t('Support')}
           </Button>
         </Stack>
         <Box flex="1 1 auto" />
-        <Stack direction="row">
-          <LanguageMenu />
-          <Button
-            variant="text"
-            color="inherit"
-            startIcon={<LoginIcon />}
-            component={NavLink}
-            to="/login"
-            sx={{ display: { xs: 'none', sm: 'flex' } }}
-          >
-            {t('Login')}
-          </Button>
-          <Button
-            variant="text"
-            color="inherit"
-            startIcon={<PersonAddIcon />}
-            component={NavLink}
-            to="/register"
-            sx={{ display: { xs: 'none', sm: 'flex' } }}
-          >
-            {t('Register')}
-          </Button>
-        </Stack>
+        {auth.status === AuthStatus.LoggedIn && auth.user ? (
+          <Stack direction="row">
+            <Button
+              variant="text"
+              color="inherit"
+              startIcon={
+                <Avatar
+                  src={auth.user.avatar?.url}
+                  alt={auth.user.name}
+                  sx={{ width: 24, height: 24 }}
+                />
+              }
+              component={NavLink}
+              to="/app/settings"
+              sx={{ display: { xs: 'none', sm: 'flex' } }}
+            >
+              {auth.user.name}
+            </Button>
+            <Button
+              variant="text"
+              color="inherit"
+              startIcon={<Dashboard />}
+              component={NavLink}
+              to="/app"
+              sx={{ display: { xs: 'none', sm: 'flex' } }}
+            >
+              {t('Dashboard')}
+            </Button>
+            <LoadingButton
+              variant="text"
+              color="inherit"
+              startIcon={<LogoutIcon />}
+              loading={logout.loading}
+              loadingPosition="start"
+              onClick={logout.submit}
+              sx={{ display: { xs: 'none', sm: 'flex' } }}
+            >
+              {t('Logout')}
+            </LoadingButton>
+          </Stack>
+        ) : (
+          <Stack direction="row">
+            <LanguageMenu />
+
+            <Button
+              variant="text"
+              color="inherit"
+              startIcon={<LoginIcon />}
+              component={NavLink}
+              to="/login"
+              sx={{ display: { xs: 'none', sm: 'flex' } }}
+            >
+              {t('Login')}
+            </Button>
+            <Button
+              variant="text"
+              color="inherit"
+              startIcon={<PersonAddIcon />}
+              component={NavLink}
+              to="/register"
+              sx={{ display: { xs: 'none', sm: 'flex' } }}
+            >
+              {t('Register')}
+            </Button>
+          </Stack>
+        )}
       </Toolbar>
     </AppBar>
   );
