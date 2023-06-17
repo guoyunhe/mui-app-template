670c65acf220b4c414a002b42d8dd87549d9ad2c
Guo Yunhe <i@guoyunhe.me>
2023-06-17 22:47:36 +0800

    left nav

--- src/layouts/landing/LeftNav.tsx
+++ src/layouts/landing/LeftNav.tsx
@@ -1,13 +1,18 @@
+import { AuthStatus, useAuth, useLogout } from '@guoyunhe/react-auth';
 import {
   AutoAwesome as AutoAwesomeIcon,
+  Dashboard as DashboardIcon,
   Login as LoginIcon,
+  Logout as LogoutIcon,
   PersonAdd as PersonAddIcon,
   Sell as SellIcon,
+  Settings as SettingsIcon,
   SupportAgent as SupportAgentIcon,
 } from '@mui/icons-material';
 import { Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { NavLink } from 'react-router-dom';
+import User from 'src/types/models/User';
 import { drawerWidth } from './config';
 
 export interface LeftNavProps {
@@ -17,6 +22,8 @@ export interface LeftNavProps {
 
 export default function LeftNav({ drawerOpen, onDrawerClose }: LeftNavProps) {
   const { t } = useTranslation();
+  const auth = useAuth<User>();
+  const logout = useLogout();
 
   return (
     <Drawer open={drawerOpen} onClose={onDrawerClose} sx={{ width: drawerWidth }}>
@@ -40,18 +47,43 @@ export default function LeftNav({ drawerOpen, onDrawerClose }: LeftNavProps) {
           <ListItemText primary={t('Support')} />
         </ListItemButton>
         <Divider />
-        <ListItemButton component={NavLink} to="/login">
-          <ListItemIcon>
-            <LoginIcon />
-          </ListItemIcon>
-          <ListItemText primary={t('Login')} />
-        </ListItemButton>
-        <ListItemButton component={NavLink} to="/register">
-          <ListItemIcon>
-            <PersonAddIcon />
-          </ListItemIcon>
-          <ListItemText primary={t('Register')} />
-        </ListItemButton>
+        {auth.status === AuthStatus.LoggedIn && auth.user ? (
+          <>
+            <ListItemButton component={NavLink} to="/app">
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary={t('Dashboard')} />
+            </ListItemButton>
+            <ListItemButton component={NavLink} to="/app/settings">
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary={t('Settings')} />
+            </ListItemButton>
+            <ListItemButton onClick={logout.submit}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary={t('Logout')} />
+            </ListItemButton>
+          </>
+        ) : (
+          <>
+            <ListItemButton component={NavLink} to="/login">
+              <ListItemIcon>
+                <LoginIcon />
+              </ListItemIcon>
+              <ListItemText primary={t('Login')} />
+            </ListItemButton>
+            <ListItemButton component={NavLink} to="/register">
+              <ListItemIcon>
+                <PersonAddIcon />
+              </ListItemIcon>
+              <ListItemText primary={t('Register')} />
+            </ListItemButton>
+          </>
+        )}
       </List>
     </Drawer>
   );
