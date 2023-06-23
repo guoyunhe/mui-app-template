d47dedd7c640e750a3f45c2938eb4cb9e268f22d
Guo Yunhe <i@guoyunhe.me>
2023-06-23 16:48:18 +0800

    remove responsiveFontSizes

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,5 @@
-import { responsiveFontSizes } from '@mui/material';
 import { createCompactTheme } from 'mui-material-compact';
 
-const theme = responsiveFontSizes(createCompactTheme({}), { factor: 3 });
+const theme = createCompactTheme({});
 
 export default theme;
