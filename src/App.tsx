3b25d59d0dfeb6fe9d6294e244e3d55ca7b11620
Guo Yunhe <i@guoyunhe.me>
2023-07-02 16:10:03 +0800

    messages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { AuthProvider } from '@guoyunhe/react-auth';
 import { CircularProgress, CssBaseline } from '@mui/material';
 import { DualThemeProvider } from 'mui-palette-mode';
 import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { RouterProvider } from 'react-router-dom';
 import LanguageEffects from './components/language-effects';
 import router from './router';
 import { darkTheme, lightTheme } from './themes';
 
 export default function App() {
+  const { t } = useTranslation();
   return (
     <Suspense
       fallback={
@@ -23,7 +25,16 @@ export default function App() {
         />
       }
     >
-      <DualThemeProvider lightTheme={lightTheme} darkTheme={darkTheme} defaultPaletteMode="auto">
+      <DualThemeProvider
+        lightTheme={lightTheme}
+        darkTheme={darkTheme}
+        defaultPaletteMode="auto"
+        messages={{
+          auto: t('Auto'),
+          dark: t('Dark'),
+          light: t('Light'),
+        }}
+      >
         <AuthProvider>
           <CssBaseline />
           <LanguageEffects />
