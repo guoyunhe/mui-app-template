b0cf850146f5b9fa681c4c950b433afb3e02af5e
Guo Yunhe <i@guoyunhe.me>
2023-08-17 21:49:15 +0800

    vite electron

---
+++ electron.vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'electron-vite';
+
+export default defineConfig({
+  main: {
+    // vite config options
+  },
+  preload: {
+    // vite config options
+  },
+  renderer: {
+    // vite config options
+  },
+});
