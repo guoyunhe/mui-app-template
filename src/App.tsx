39085561ecdc498b3f6cca7f4b4b8ef462f55620
Guo Yunhe <i@guoyunhe.me>
2023-08-20 12:57:20 +0800

    css baseline color

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ export default function App() {
           }}
         >
           <AuthProvider>
-            <CssBaseline />
+            <CssBaseline enableColorScheme />
             <LanguageEffects />
             <RouterProvider router={router} />
           </AuthProvider>
