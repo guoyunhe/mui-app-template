e91ba0e5f61e63d41bc6674f4b21f32ffe8008ce
Guo Yunhe <i@guoyunhe.me>
2023-11-12 22:45:23 +0800

    roboto fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import '~/bootstrap/axios';
 import '~/bootstrap/i18n';
-import App from './App';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '~/App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
