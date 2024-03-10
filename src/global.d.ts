6a0e1878692aa5d2babdba3ec4161e54804c26f1
Guo Yunhe <i@guoyunhe.me>
2024-03-10 13:59:36 +0800

    remove electron

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,7 @@
-import { ElectronAPI } from '@electron-toolkit/preload';
-
 declare global {
   interface Window {
-    electron: ElectronAPI;
-    api: unknown;
     SOME_GLOBAL_VARIABLE: string;
   }
 }
 
-export {}; // must have this empty export to take effect
+export { }; // must have this empty export to take effect
