1b6e6523b93db962c268f57a81639ed0ce178620
Guo Yunhe <i@guoyunhe.me>
2025-02-22 18:31:46 +0800

    chore(vite): update vite and vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
-import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
+
+import react from '@vitejs/plugin-react-swc';
 import adsense from 'vite-plugin-adsense';
 import { ViteEjsPlugin as ejs } from 'vite-plugin-ejs';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const env = loadEnv('', process.cwd());
 
-// https://vitejs.dev/config/
+/** @see https://vitejs.dev/config/ */
 export default defineConfig({
   server: {
     host: true,
@@ -15,13 +16,13 @@ export default defineConfig({
     open: true,
   },
   plugins: [react(), tsconfigPaths(), ejs(env), adsense()],
-  // https://vitest.dev/config/
+  /** @see https://vitest.dev/config/ */
   test: {
     coverage: {
       reporter: ['text', 'json', 'html'],
     },
     globals: true,
-    environment: 'jsdom',
+    environment: 'happydom',
     setupFiles: './src/setupTests.ts',
   },
 });
